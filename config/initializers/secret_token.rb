de4571e6aafce6129bbdeb4072b2615cd0f37d1c
shagmbg <michael.b.gottfried@gmail.com>
2013-11-04 13:09:25 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tpsociety::Application.config.secret_key_base = '068677e6e03b1a6bada2e5c8b884c60975fefc97733f8ebc469aceb5e4e37fd605b6ca7123ba6b834cdb22b4f568aaa661a71367178e8609cc4feb35eae6ac55'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tpsociety::Application.config.session_store :cookie_store, key: '_tpsociety_session'
