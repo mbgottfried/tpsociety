6f367ccf3b591148967ff23e94113c34de3d1f2d
shagmbg <michael.b.gottfried@gmail.com>
2013-11-06 09:54:17 -0500

    added plans to sign up

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,2 @@
-Rails.configuration.stripe = {
-  :publishable_key => ENV['PUBLISHABLE_KEY'],
-  :secret_key      => ENV['SECRET_KEY']
-}
-
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+Stripe.api_key = "sk_test_cgajMxpMgtzyEy4fdPKC9zzd"
+STRIPE_PUBLISHABLE_KEY = "pk_test_ybms4ZSc8kP1FnekvyTbp7Wn"
